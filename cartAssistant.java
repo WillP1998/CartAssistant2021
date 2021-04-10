374083d21491867c3662cc56d5d76f4c01f968ed
William Pano <72896979+WillP1998@users.noreply.github.com>
2021-04-10 00:01:06 -0500

    Update cartAssistant.java

--- cartAssistant.java
+++ cartAssistant.java
@@ -33,52 +33,60 @@ public class cartAssistant {
 		prodUrl.setBounds(90, 20, 300, 25);
 		panel.add(prodUrl);
 		
+		JLabel contactInfo = new JLabel("Contact Info");
+		contactInfo.setBounds(10, 50, 80, 25);
+		panel.add(contactInfo);
+		
 		JLabel userName = new JLabel("Name: ");
-		userName.setBounds(30, 50, 80, 25);
+		userName.setBounds(30, 80, 80, 25);
 		panel.add(userName);
 		
 		JTextField name = new JTextField();
-		name.setBounds(80, 50, 150, 25);
+		name.setBounds(80, 80, 150, 25);
 		panel.add(name);
 		
 		JLabel email = new JLabel("Email: ");
-		email.setBounds(30, 80, 300, 25);
+		email.setBounds(30, 110, 300, 25);
 		panel.add(email);
 		
 		JTextField emailAddress = new JTextField();
-		emailAddress.setBounds(80, 80, 300, 25);
+		emailAddress.setBounds(80, 110, 300, 25);
 		panel.add(emailAddress);
 		
 		JLabel telephone = new JLabel("Phone Number: ");
-		telephone.setBounds(240, 50, 110, 25);
+		telephone.setBounds(240, 80, 110, 25);
 		panel.add(telephone);
 		
 		JTextField phone = new JTextField();
-		phone.setBounds(340, 50, 130, 25);
+		phone.setBounds(340, 80, 130, 25);
 		panel.add(phone);
 		
+		JLabel Address = new JLabel("Address");
+		Address.setBounds(10, 140, 120, 25);
+		panel.add(Address);
+		
 		JLabel streetAddress = new JLabel("Street Address: ");
-		streetAddress.setBounds(10, 110, 120, 25);
+		streetAddress.setBounds(10, 170, 120, 25);
 		panel.add(streetAddress);
 		
 		JTextField shippingAddress = new JTextField();
-		shippingAddress.setBounds(130, 110, 180, 25);
+		shippingAddress.setBounds(130, 170, 180, 25);
 		panel.add(shippingAddress);
 		
 		JLabel zipCode = new JLabel("Zip Code: ");
-		zipCode.setBounds(20, 140, 80 ,25);
+		zipCode.setBounds(20, 200, 80 ,25);
 		panel.add(zipCode);
 		
 		JTextField zip = new JTextField();
-		zip.setBounds(130, 140, 80, 25);
+		zip.setBounds(130, 200, 80, 25);
 		panel.add(zip);
 		
 		JLabel city = new JLabel("City: ");
-		city.setBounds(20, 170, 80, 25);
+		city.setBounds(20, 230, 80, 25);
 		panel.add(city);
 		
 		JTextField orderCity = new JTextField();
-		orderCity.setBounds(50, 170, 80, 25);
+		orderCity.setBounds(130, 230, 80, 25);
 		panel.add(orderCity);
 		
 			
